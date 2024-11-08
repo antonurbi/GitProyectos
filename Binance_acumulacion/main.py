e25fc45de291a5269e5059cb388237ef4ba30a76
antonurbi <148626327+antonurbi@users.noreply.github.com>
2024-11-08 19:01:37 +0100

    Update main.py

--- Binance_acumulacion/main.py
+++ Binance_acumulacion/main.py
@@ -8,5 +8,5 @@ interval = Client.KLINE_INTERVAL_1DAY
 lookback = '30 day ago UTC'
 dias = 1 # Días entre inversiones
 inversion = 100 # Cantidad a invertir cada 30 días
-# data.get_data(symbol, interval, lookback)
+data.get_data(symbol, interval, lookback)
 rendimiento.rendimiento(inversion,symbol,inversion, dias)
